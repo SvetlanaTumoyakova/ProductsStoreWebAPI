11bd27867fffdfd64ea29350c6b2434d49cf379f
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2025-11-13 19:08:51 +0700

    feat: add product update controller

---
+++ ProductStoreWebAPI/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductsStore.DAL;
+using ProductsStore.WebAPI.Providers;
+
+namespace ProductsStore.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : Controller
+    {
+        private readonly DataBaseContext _context;
+        private readonly ProductProvider _productProvider;
+
+        public ProductController(DataBaseContext context, ProductProvider productProvider)
+        {
+            _context = context;
+            _productProvider = productProvider;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productProvider.GetProducts();
+            return Ok(products);
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> Get(Guid id)
+        {
+            var product = await _productProvider.GetProductById(id);
+
+            if (product == null) return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpGet("search")]
+        public async Task<IActionResult> Search([FromQuery(Name = "q")] string? q)
+        {
+            var products = await _productProvider.SearchProductsByNameAsync(q);
+            return Ok(products);
+        }
+
+    }
+}
